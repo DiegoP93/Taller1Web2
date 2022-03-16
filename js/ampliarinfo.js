833fb05fe720f8cd2fcf3c72234247f88e8e14d8
Diego <jpamplonasanchez@gmail.com>
2022-03-16 18:00:39 -0500

    WB2 se llena contenido en el index y se avanza la tienda

--- js/ampliarinfo.js
+++ js/ampliarinfo.js
@@ -3,7 +3,10 @@ export function ampliarInformacion(event){
     let producto={}
     if(event.target.classList.contains("btn")){
         producto={
-            foto:event.target.parentElement.querySelector("img").src
+            foto:event.target.parentElement.querySelector("img").src,
+            nombre:event.target.parentElement.querySelector("h4").textContent,
+            precio:event.target.parentElement.querySelector("h5").textContent,
+            descripción:event.target.parentElement.querySelector("h6").textContent
             
         }
         console.log(producto)
@@ -11,7 +14,12 @@ export function ampliarInformacion(event){
         let foto=document.getElementById("fotoinfo")
         foto.src=producto.foto
         let nombre=document.getElementById("nombreinfo")
-        nombre.textContent=producto.content
+        nombre.textContent=producto.nombre
+        let precio=document.getElementById("precioinfo")
+        precio.textContent=producto.precio
+        let descripción=document.getElementById("descripcioninfo")
+        descripción.textContent=document.producto.descripción
     }
+    
 
 }
--- js/controlador.js
+++ js/controlador.js
@@ -1,28 +1,3 @@
-/*let nombreUsuario="Juan Diego"
-let estatura=1.71
-let edad=18
-let telefono="3154235625"
-let esPaisa=false 
-
-const APELLIDOS_USUARIO="Sanchez"
-
-//console.log("Buenas tardes",nombreUsuario,"Su altura es:",estatura,"Su edad es:",edad)
-
-/*console.log(`Buenas tardes ${nombreUsuario} ${APELLIDOS_USUARIO} su edad es: ${edad} su estatura es: ${estatura} su telefono es: ${telefono} Es Paisa? ${esPaisa}
-`) //
-
-let etiquetaTitulo=document.getElementById("titulo")
-etiquetaTitulo.textContent="Crack"
-etiquetaTitulo.classList.add("text-danger")
-etiquetaTitulo.classList.add("text-center")
-
-
-let etiquetaIcono=document.getElementById("icono")
-etiquetaIcono.textContent="Pp93"
-
-let etiquetaFotokratos=document.getElementById("fotokratos")
-etiquetaFotokratos.src="img/fondokratos.jpg"*/
-
 import { pintarTienda } from "./llenadoTienda.js";
 pintarTienda()
 import{ampliarInformacion} from "./ampliarinfo.js"
