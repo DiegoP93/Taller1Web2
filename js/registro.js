671df17d450f17192231b379ae49896006244fc0
AngieVarela <paola23710@gmail.com>
2022-04-23 11:37:04 -0500

    WB2 se termina login

--- js/registro.js
+++ js/registro.js
@@ -1,6 +1,7 @@
 import {getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
 
 let botonregistro=document.getElementById("botonregistro")
+let modalinformativo=new bootstrap.Modal(document.getElementById('modalinformativo'))
 
 botonregistro.addEventListener("click",function(event){
 
@@ -15,14 +16,26 @@ botonregistro.addEventListener("click",function(event){
     // Signed in
     const user = userCredential.user;
 
-    console.log("registro")
+    let textoinformativo=document.getElementById("informaciongeneral")
+    let formulario=document.getElementById("formularioderegistro")
+    formulario.reset()
+    textoinformativo.textContent="Exitoso quedo registrado"
+    modalinformativo.show()
+    setTimeout(function(){
+      modalinformativo.hide()
+    },2000)
     })
      // ...
    .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
     // ..
-    console.log("registro malo")
+    let textoinformativo=document.getElementById("informaciongeneral")
+    textoinformativo.textContent="fallo"+errorMessage
+    modalinformativo.show()
+    setTimeout(function(){
+      modalinformativo.hide()
+    },2000)
     });
 
 })
